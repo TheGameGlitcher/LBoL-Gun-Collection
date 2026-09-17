57545a7c1e353ff85b864e1be6a5c5543ff632d7
TheGameGlitcher <gabefgarner@windstream.net>
2026-09-17 04:02:18 -0400

    Added Yukari basic attack guns
    Added the guns for Yukari's two basic attacks; Danmaku from Above and Danmaku from Below.

---
+++ Yukari/DanmakuAbove/DanmakuAboveB/DanmakuAboveBPiecesDef.cs
@@ -0,0 +1,141 @@
+﻿using LBoL.ConfigData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestMod.Source.Config;
+using TestMod.Source.Guns.Pieces.Template;
+
+namespace TestMod.Source.Guns.Pieces.Yukari.DanmakuAbove
+{
+    public sealed class DanmakuAboveBPiece1Def : TestModPiece
+    {
+        public override PieceConfig MakeConfig()
+        {
+            PieceConfig config = TestModDefaultConfig.DefaultPieceConfig();
+
+            config.Id = ConvertGunId(178003);
+
+            config.Projectile = "DanmaBigMagic";
+
+            config.ShootType = 0;
+
+            config.LastWave = false;
+
+            config.HitAmount = 0;
+
+            config.ZeroHitNotDie = false;
+
+            config.Scale = new float[][] { new float[] { 1f } };
+
+            config.Color = new int[][] { new int[] { (int)BulletColor.Grayscale } };
+
+            config.Aim = 1;
+
+            config.RootType = 1;
+
+            config.X = new float[][] { new float[] { -2.3f, 0.2f } };
+
+            config.Y = new float[][] { new float[] { 2.3f, 0.2f } };
+
+            config.StartTime = 0;
+
+            config.Group = 1;
+
+            config.Way = new int[][] { new int[] { 1 } };
+
+            config.Life = new int[][] { new int[] { 75 } };
+
+            config.StartSpeed = new float[][] { new float[] { 4f } };
+
+            config.VanishV3 = new UnityEngine.Vector3(1f, 1f, 1f);
+
+            return config;
+        }
+    }
+
+    public sealed class DanmakuAboveBPiece2Def : TestModPiece
+    {
+        public override PieceConfig MakeConfig()
+        {
+            PieceConfig config = TestModDefaultConfig.DefaultPieceConfig();
+
+            config.Id = ConvertGunId(178003, 1);
+
+            config.Projectile = "DanmaKunai";
+
+            config.ShootType = 2;
+
+            config.ParentPiece = 0;
+
+            config.LastWave = true;
+
+            config.ZeroHitNotDie = false;
+
+            config.Scale = new float[][] { new float[] { 1f } };
+
+            config.Color = new int[][] { new int[] { (int)BulletColor.Pink } };
+
+            config.Aim = 1;
+
+            config.StartTime = 0;
+
+            config.GInterval = 13;
+
+            config.Group = 6;
+
+            config.Way = new int[][] { new int[] { 12 } };
+
+            config.Range = new float[][] { new float[] { 360f } };
+
+            config.Life = new int[][] { new int[] { 60 } };
+
+            config.StartSpeed = new float[][] { new float[] { 6f } };
+
+            return config;
+        }
+    }
+
+    public sealed class DanmakuAboveBPiece3Def : TestModPiece
+    {
+        public override PieceConfig MakeConfig()
+        {
+            PieceConfig config = TestModDefaultConfig.DefaultPieceConfig();
+
+            config.Id = ConvertGunId(178003, 2);
+
+            config.Projectile = "DanmaKunai";
+
+            config.ShootType = 2;
+
+            config.ParentPiece = 0;
+
+            config.LastWave = true;
+
+            config.ZeroHitNotDie = false;
+
+            config.Scale = new float[][] { new float[] { 1f } };
+
+            config.Color = new int[][] { new int[] { (int)BulletColor.YellowGreen } };
+
+            config.Aim = 1;
+
+            config.StartTime = 0;
+
+            config.GInterval = 12;
+
+            config.Group = 6;
+
+            config.Way = new int[][] { new int[] { 12 } };
+
+            config.Range = new float[][] { new float[] { 360f } };
+
+            config.GAngle = new float[][] { new float[] { -1f } };
+
+            config.Life = new int[][] { new int[] { 60 } };
+
+            config.StartSpeed = new float[][] { new float[] { 3.5f } };
+
+            return config;
+        }
+    }
+}
